e837fb5042fd177076f8161256ccaf97f05fc353
Gafar Popoola <gafar.popoola@venturegardengroup.com>
2021-11-17 09:18:42 +0100

    tiny clean up

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,6 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log('Questions ==>', questions)
   const startTrivia = async () => {
     setLoading(true);
     setGameOver(false);
